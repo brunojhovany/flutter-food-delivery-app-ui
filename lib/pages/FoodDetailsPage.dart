4e486b28a8976a735f835ed6aabaa6b315685eed
Jhovany Morales Bruno <jhovanymb.info@gmail.com>
2023-08-24 11:27:52 -0600

    dart 3 support

--- lib/pages/FoodDetailsPage.dart
+++ lib/pages/FoodDetailsPage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_app/animation/ScaleRoute.dart';
 import 'package:flutter_app/pages/FoodOrderPage.dart';
-import 'package:flutter_app/widgets/FoodDetailsSlider.dart';
 
 class FoodDetailsPage extends StatefulWidget {
   @override
@@ -25,7 +25,6 @@ class _FoodDetailsPageState extends State<FoodDetailsPage> {
             ),
             onPressed: () => Navigator.of(context).pop(),
           ),
-          brightness: Brightness.light,
           actions: <Widget>[
             IconButton(
                 icon: Icon(
@@ -36,6 +35,7 @@ class _FoodDetailsPageState extends State<FoodDetailsPage> {
                   Navigator.push(context, ScaleRoute(page: FoodOrderPage()));
                 })
           ],
+          systemOverlayStyle: SystemUiOverlayStyle.dark,
         ),
         body: Container(
           padding: EdgeInsets.only(
@@ -126,10 +126,10 @@ class FoodTitleWidget extends StatelessWidget {
   String productHost;
 
   FoodTitleWidget({
-    Key key,
-    @required this.productName,
-    @required this.productPrice,
-    @required this.productHost,
+    Key? key,
+    required this.productName,
+    required this.productPrice,
+    required this.productHost,
   }) : super(key: key);
 
   @override
